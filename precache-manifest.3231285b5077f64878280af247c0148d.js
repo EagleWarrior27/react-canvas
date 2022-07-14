98ce2e06b6a77306f72c4e67f50428c6e4e6b36e
CarlosUcan <carlos.ucan@chapur.com>
2022-07-14 17:39:48 -0500

    Updates

---
+++ precache-manifest.3231285b5077f64878280af247c0148d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "758485350285bd2a97ab",
+    "url": "/react-canvas/static/css/main.bbf788b8.chunk.css"
+  },
+  {
+    "revision": "758485350285bd2a97ab",
+    "url": "/react-canvas/static/js/main.75848535.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/react-canvas/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "c60e0baf773324a0b80b",
+    "url": "/react-canvas/static/js/2.c60e0baf.chunk.js"
+  },
+  {
+    "revision": "09e582a21014616cd4a9",
+    "url": "/react-canvas/static/js/runtime~main.09e582a2.js"
+  },
+  {
+    "revision": "7a5961fe67365d53814c0b5bff120f21",
+    "url": "/react-canvas/index.html"
+  }
+];
